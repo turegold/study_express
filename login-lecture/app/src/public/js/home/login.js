c7d6c8ed2684ddf2c32ea89d07674e32a4e2e0c8
LYM <72262985+turegold@users.noreply.github.com>
2024-02-12 10:05:25 +0000

    돔을 이용하여 HTML 객체 저장

--- login-lecture/app/src/public/js/home/login.js
+++ login-lecture/app/src/public/js/home/login.js
@@ -1,3 +1,16 @@
 "use strict"
 
-console.log("hello!!!!");
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+
+    console.log(req);
+}
