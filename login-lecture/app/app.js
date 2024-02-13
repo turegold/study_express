c59a6f2736d582cd855f656a998ce1363805aa57
LYM <72262985+turegold@users.noreply.github.com>
2024-02-13 13:45:48 +0000

    로그인 인증 기능 만들기

--- login-lecture/app/app.js
+++ login-lecture/app/app.js
@@ -1,6 +1,7 @@
 "use strict";
 //모듈
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 //라우팅
 const home = require("./src/routes/home");
@@ -9,9 +10,9 @@ const home = require("./src/routes/home");
 app.set("views", "/workspaces/study_express/login-lecture/app/src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extened: true}));
 app.use("/",home); // use -> 미들 웨어를 등록해주는 메소드
 
 
 module.exports = app;
-
--- login-lecture/app/src/routes/home/home.ctrl.js
+++ login-lecture/app/src/routes/home/home.ctrl.js
@@ -1,15 +1,47 @@
 "use strict";
 
-const hello = (req,res)=>{
-    res.render("home/index")
+
+
+const output = {
+    home : (req,res)=>{
+        res.render("home/index")
+    },
+    
+    login : (req,res)=>{
+        res.render("home/login")
+    },
 };
 
-const login = (req,res)=>{
-    res.render("home/login")
+const users = {
+    id: ["woorimIT", "나개발", "김팅장"],
+    password: ["1234", "1234", "12345"],
 };
+
+const process = {
+    login: (req,res)=>{
+        const id = req.body.id,
+        password = req.body.password
+
+        if (users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.password[idx] === password){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하여습니다."
+        });
+    },
+};
+
+
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process,
 }
 
